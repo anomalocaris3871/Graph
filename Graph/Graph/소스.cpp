867472459eac6340716f42d5a3e4a41c3cac4e3a
Lee <anomalocaris1@naver.com>
2020-12-14 21:35:52 +0900

    2020/12/14

---
+++ Graph/Graph/소스.cpp
@@ -0,0 +1,19 @@
+#include "AdjMatGraph.h"
+
+int main() {
+	AdjMatGraph g;
+
+	for (int i = 0; i < 4;++i) {
+		g.insertVertex('A' + i);
+	}
+	g.insertEdge(0, 1);
+	g.insertEdge(0, 3);
+	g.insertEdge(1, 2);
+	g.insertEdge(1, 3);
+	g.insertEdge(2, 3);
+	printf("The Graph that is maded by Adjacent Array\n");
+	g.display();
+
+	return 0;
+}
+
